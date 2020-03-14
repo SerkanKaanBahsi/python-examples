645966be86c99850bdb28586597407119cfb68ce
Mert <mertturkmenoglu99@gmail.com>
2020-03-14 04:14:02 +0300

    Code cleanup in oop folder

--- oop/Overload.py
+++ oop/Overload.py
@@ -1,25 +1,27 @@
 class Space:
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
     def __str__(self):
-        return "({0},{1})".format(self.x,self.y)
+        return "({0},{1})".format(self.x, self.y)
+
     # overloading the "+" operator
-    def __add__(self,other):
-        x=self.x+other.x
-        y=self.y+other.y
-        return Space(x,y)
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Space(x, y)
 
 
-
-new_Space=Space(5,3)
-second_Space=Space(7,12)
+new_Space = Space(5, 3)
+second_Space = Space(7, 12)
 print(new_Space)
-#same with
+
+# same with
 print(str(new_Space))
 print(format(new_Space))
-#it actually does new_Space.__str__()
+# it actually does new_Space.__str__()
 
-print(new_Space+second_Space)
+print(new_Space + second_Space)
 
-#You can overload other operators as well even the comparision operators
+# You can overload other operators as well even the comparision operators
